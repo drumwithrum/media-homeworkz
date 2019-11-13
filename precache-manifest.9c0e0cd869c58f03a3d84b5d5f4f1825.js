9d9d9bbeca34ef15c4ee88e3ca37d88857a01440
Wojciech Bębenek <drumwithrum@outlook.com>
2019-11-13 19:21:43 +0100

    Updates

---
+++ precache-manifest.9c0e0cd869c58f03a3d84b5d5f4f1825.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "733f19065993ca3c95814cab10271747",
+    "url": "/media-homeworkz/index.html"
+  },
+  {
+    "revision": "2728c42edc4d7f1405de",
+    "url": "/media-homeworkz/static/css/main.ea1ff000.chunk.css"
+  },
+  {
+    "revision": "01fd377ed970871f4dd0",
+    "url": "/media-homeworkz/static/js/2.d6875810.chunk.js"
+  },
+  {
+    "revision": "2728c42edc4d7f1405de",
+    "url": "/media-homeworkz/static/js/main.b5824232.chunk.js"
+  },
+  {
+    "revision": "60573535b8bc9ae19a46",
+    "url": "/media-homeworkz/static/js/runtime-main.dbc45842.js"
+  }
+]);
